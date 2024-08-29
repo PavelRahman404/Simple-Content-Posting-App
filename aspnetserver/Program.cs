831eb6b0a5e998596e5a363ba0407bb22ad9897c
PavelRahman404 <paveltauker@gmail.com>
2024-08-29 19:22:45 +0600

    Finalising Endpoints

--- aspnetserver/Program.cs
+++ aspnetserver/Program.cs
@@ -16,6 +16,58 @@ app.UseHttpsRedirection();
 
 app.MapGet("/get-all-posts", async () => await PostRepository.GetPostsAsync()).WithTags("Post Endpoints");
 
+app.MapGet("/get-post-by-id/{postId}", async (int postId) =>
+{
+    Post postToReturn = await PostRepository.GetPostsAsyncById(postId);
+    if (postToReturn != null)
+    {
+        return Results.Ok(postToReturn);
+    }
+    else
+    {
+        return Results.BadRequest();
+    }
+}).WithTags("Post Endpoints");
+
+app.MapPost("/create-post", async (Post postToCreate) =>
+{
+    bool createSuccessful = await PostRepository.CreatePostsAsync(postToCreate);
+    if (createSuccessful)
+    {
+        return Results.Ok("Create Successful");
+    }
+    else
+    {
+        return Results.BadRequest();
+    }
+}).WithTags("Posts Endpoints");
+
+app.MapPut("/update-post", async (Post postToUpdate) =>
+{
+    bool updateSuccessful = await PostRepository.UpdatePostsAsync(postToUpdate);
+    if (updateSuccessful)
+    {
+        return Results.Ok("Update Successful");
+    }
+    else
+    {
+        return Results.BadRequest();
+    }
+}).WithTags("Posts Endpoints");
+
+app.MapDelete("/delete-post-by-id/{postId}", async (int postId) =>
+{
+    bool deleteSuccessful = await PostRepository.DeletePostsAsync(postId);
+    if (deleteSuccessful)
+    {
+        return Results.Ok("Delete Successful");
+    }
+    else
+    {
+        return Results.BadRequest();
+    }
+}).WithTags("Posts Endpoints");
+
 app.Run();
 
 
